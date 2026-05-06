0a4a014990f02a469074b9f7d8383bf6d9f95e87
joryfahad234 <445052234@std.psau.edu.sa>
2026-05-06 21:36:47 +0300

    Added ReentrantLock for counters

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 // ANSI Color Codes for enhanced terminal output
 class Colors {
@@ -38,28 +39,39 @@ class SharedResources {
     
     // TODO #1: Add a ReentrantLock(s) here to protect critical sections
     // Example: public static final ReentrantLock lock = new ReentrantLock();
-    
+    public static final ReentrantLock counterLock = new ReentrantLock();
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
     
     // Method to increment context switch counter
-    public static void incrementContextSwitch() {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: Multiple threads might read and write simultaneously!
+public static void incrementContextSwitch() {
+    counterLock.lock();
+    try {
         contextSwitchCount++;
+    } finally {
+        counterLock.unlock();
     }
+}
     
     // Method to increment completed process counter
-    public static void incrementCompletedProcess() {
-        // TODO: Protect this critical section with a lock
+  public static void incrementCompletedProcess() {
+    counterLock.lock();
+    try {
         completedProcessCount++;
+    } finally {
+        counterLock.unlock();
     }
+}
     
     // Method to add waiting time
-    public static void addWaitingTime(long time) {
-        // TODO: Protect this critical section with a lock
+public static void addWaitingTime(long time) {
+    counterLock.lock();
+    try {
         totalWaitingTime += time;
+    } finally {
+        counterLock.unlock();
     }
+}
     
     // Method to log execution
     public static void logExecution(String message) {
